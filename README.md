d841ff981c3d0c4d236d05c613377c3f909c68a6
wang-q <wang-q@outlook.com>
2018-03-16 04:42:00 +0800

    Init commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+App::Egaz - It's new $module
+
+# SYNOPSIS
+
+    use App::Egaz;
+
+# DESCRIPTION
+
+App::Egaz is ...
+
+# LICENSE
+
+Copyright (C) wang-q.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+wang-q <wang-q@outlook.com>
---
+++ lib/App/Egaz.pm
@@ -0,0 +1,39 @@
+package App::Egaz;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+App::Egaz - It's new $module
+
+=head1 SYNOPSIS
+
+    use App::Egaz;
+
+=head1 DESCRIPTION
+
+App::Egaz is ...
+
+=head1 LICENSE
+
+Copyright (C) wang-q.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+wang-q E<lt>wang-q@outlook.comE<gt>
+
+=cut
+
