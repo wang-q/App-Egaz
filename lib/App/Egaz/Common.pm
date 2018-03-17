4fedeb014a7b09e5995ffa401095dfdd5577d80b
wang-q <wang-q@outlook.com>
2018-03-17 16:14:06 +0800

    Add resolve_file() to Common.pm

--- lib/App/Egaz/Common.pm
+++ lib/App/Egaz/Common.pm
@@ -14,5 +14,31 @@ use Path::Tiny qw();
 use Template;
 use YAML::Syck qw();
 
+use App::Fasops::Common;
+
+sub resolve_file {
+    my $original = shift;
+    my @pathes   = @_;
+
+    my $file = $original;
+    if ( !-e $file ) {
+        $file = Path::Tiny::path($file)->basename();
+
+        if ( !-e $file ) {
+            for my $p (@pathes) {
+                $file = Path::Tiny::path($p)->child($file);
+                last if -e $file;
+            }
+        }
+    }
+
+    if ( -e $file ) {
+        return $file->stringify;
+    }
+    else {
+        Carp::confess "Can't resole file for [$original]\n";
+    }
+}
+
 1;
 
