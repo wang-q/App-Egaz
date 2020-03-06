7d3866b782890ead3fc0f7a72393151ac8e5e6d2
wang-q <wang-q@outlook.com>
2020-03-07 02:57:00 +0800

    Misc changes

--- lib/App/Egaz/Command/template.pm
+++ lib/App/Egaz/Command/template.pm
@@ -1211,9 +1211,9 @@ cat axt.gl.fasta > axt.all.fasta
 #----------------------------#
 echo >&2 "==> Get more paralogs"
 egaz blastn axt.gl.fasta genome.fa -o axt.bg.blast --parallel [% opt.parallel2 %]
-egaz blastmatch axt.bg.blast -c 0.95 --perchr -o axt.bg.region --parallel [% opt.parallel2 %]
+egaz blastmatch axt.bg.blast -c 0.95 -o axt.bg.region --parallel [% opt.parallel2 %]
 samtools faidx genome.fa -r axt.bg.region --continue |
-    perl -p -e '/^>/ and s/:/(+):/' \
+    perl -p -e "/^>/ and s/:/(+):/" \
     > axt.bg.fasta
 
 cat axt.gl.fasta axt.bg.fasta |
