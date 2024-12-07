9a59a53867d73d40481737b6c15a35813d1ed845
Qiang Wang <wang-q@outlook.com>
2024-12-07 20:02:40 +0800

    Misc. changes

--- lib/App/Egaz.pm
+++ lib/App/Egaz.pm
@@ -1,6 +1,6 @@
 package App::Egaz;
 
-our $VERSION = "0.2.7";
+our $VERSION = "0.2.8";
 
 use strict;
 use warnings;
@@ -56,9 +56,9 @@ chaining mechanism (C<C=2>) instead.
 
 =head1 INSTALLATION
 
-    cpanm --installdeps https://github.com/wang-q/App-Egaz/archive/0.2.6.tar.gz
+    cpanm --installdeps https://github.com/wang-q/App-Egaz/archive/0.2.8.tar.gz
     curl -fsSL https://raw.githubusercontent.com/wang-q/App-Egaz/master/share/check_dep.sh | bash
-    cpanm -nq https://github.com/wang-q/App-Egaz/archive/0.2.6.tar.gz
+    cpanm -nq https://github.com/wang-q/App-Egaz/archive/0.2.8.tar.gz
     # cpanm -nq https://github.com/wang-q/App-Egaz.git
 
 =head1 CONTAINER
